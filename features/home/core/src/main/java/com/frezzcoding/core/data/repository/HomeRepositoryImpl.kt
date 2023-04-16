55acb8c2b58cacbd6d2bd545a28c45d74548cc43
oskarlasota <frezzyysmite@gmail.com>
2023-04-16 10:23:42 +0100

    remove package name

--- features/home/core/src/main/java/com/frezzcoding/core/data/repository/HomeRepositoryImpl.kt
+++ features/home/core/src/main/java/com/frezzcoding/core/data/repository/HomeRepositoryImpl.kt
@@ -1,12 +1,13 @@
 package com.frezzcoding.core.data.repository
 
+import com.frezzcoding.core.domain.repo.HomeRepository
 import com.frezzcoding.network.AdModel
 import com.frezzcoding.network.SampleData
 import javax.inject.Inject
 
 //todo sampledata is temporary
 class HomeRepositoryImpl @Inject constructor(private val dataSource: SampleData) :
-    com.frezzcoding.core.domain.repo.HomeRepository {
+    HomeRepository {
 
     override fun getAds(): List<AdModel> {
         return dataSource.advertisementsSample
