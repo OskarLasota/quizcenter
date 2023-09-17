53cfabdfea7d0731a6d84909e072fa4e1ac9f7a1
oskarlasota <frezzyysmite@gmail.com>
2023-09-17 10:39:23 +0100

    creating quiz item composable and showing it on homefeed

--- presentation/ui/src/main/java/com/frezzcoding/ui/components/HomeFeed.kt
+++ presentation/ui/src/main/java/com/frezzcoding/ui/components/HomeFeed.kt
@@ -4,12 +4,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import com.frezzcoding.domain.AdDetails
+import com.frezzcoding.domain.QuizDetails
 
 @Composable
-fun HomeFeed(ads: List<AdDetails>, onItemPressed: () -> Unit) {
-   LazyColumn {
-       items(ads) { ad ->
-           AdItem(ad, onItemPressed)
-       }
-   }
+fun HomeFeed(items: List<Any>, onItemPressed: () -> Unit) {
+    LazyColumn {
+        items(items) { item ->
+            when (item) {
+                is AdDetails -> {
+                    AdItem(item, onItemPressed)
+                }
+                is QuizDetails -> {
+                    QuizItem(item, onItemPressed)
+                }
+            }
+        }
+    }
 }
