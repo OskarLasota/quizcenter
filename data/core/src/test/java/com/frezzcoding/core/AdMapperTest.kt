f6e6d4f567f651037d28343848a50dcae8f2378a
oskarlasota <frezzyysmite@gmail.com>
2023-09-25 21:33:28 +0100

    create temple unit tests for ad mapper

---
+++ data/core/src/test/java/com/frezzcoding/core/AdMapperTest.kt
@@ -0,0 +1,36 @@
+package com.frezzcoding.core
+
+import org.junit.Test
+
+class AdMapperTest {
+
+
+    val underTest = AdMapper()
+
+
+    @Test
+    fun `when dto id is null then returns null`() {
+
+    }
+
+    @Test
+    fun `when dto position is null then returns null`() {
+
+    }
+
+    @Test
+    fun `when dto text is null then returns null`() {
+
+    }
+
+    @Test
+    fun `when dto adOwner is null then returns null`() {
+
+    }
+
+    @Test
+    fun `when dto is correct then returns mapped domain model`() {
+
+    }
+
+}
