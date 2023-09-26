f2a2d70d34c1c20c434db6ebc6c62d1253daeaa9
oskarlasota <frezzyysmite@gmail.com>
2023-09-26 21:23:49 +0100

    add tests for ad mapper

--- data/core/src/test/java/com/frezzcoding/core/AdMapperTest.kt
+++ data/core/src/test/java/com/frezzcoding/core/AdMapperTest.kt
@@ -1,37 +1,41 @@
 package com.frezzcoding.core
 
 import com.frezzcoding.core.mappers.AdMapper
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 
 class AdMapperTest {
 
 
-    val underTest = AdMapper()
+    private val underTest = AdMapper()
 
 
     @Test
     fun `when dto id is null then returns null`() {
-
+        assertNull(underTest.apply(correctAdDto.copy(id = null)))
     }
 
     @Test
     fun `when dto position is null then returns null`() {
-
+        assertNull(underTest.apply(correctAdDto.copy(position = null)))
     }
 
     @Test
     fun `when dto text is null then returns null`() {
-
+        assertNull(underTest.apply(correctAdDto.copy(text = null)))
     }
 
     @Test
     fun `when dto adOwner is null then returns null`() {
-
+        assertNull(underTest.apply(correctAdDto.copy(adOwner = null)))
     }
 
     @Test
     fun `when dto is correct then returns mapped domain model`() {
+        val result = underTest.apply(correctAdDto)
 
+        assertEquals(correctAdDomainModel, result)
     }
 
 }
---
+++ data/core/src/test/java/com/frezzcoding/core/AdTestDto.kt
@@ -0,0 +1,28 @@
+package com.frezzcoding.core
+
+import com.frezzcoding.domain.models.ad.AdDetails
+import com.frezzcoding.domain.models.ad.AdOwner
+import com.frezzcoding.network.ads.AdDto
+import com.frezzcoding.network.ads.AdOwnerDto
+
+val correctAdDto
+    get() = AdDto(
+        id = 0,
+        position = 0,
+        text = "ad",
+        adOwner = correctAdOwnerDto
+    )
+
+
+val correctAdOwnerDto
+    get() = AdOwnerDto(
+        name = "ad owner"
+    )
+
+val correctAdDomainModel
+    get() = AdDetails(
+        id = 0,
+        position = 0,
+        text = "ad",
+        adOwner = AdOwner(name = "ad owner")
+    )
