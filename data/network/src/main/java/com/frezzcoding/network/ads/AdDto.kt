c2839418e8fbbbcff0b54a4fe6804f8c2051d995
oskarlasota <frezzyysmite@gmail.com>
2023-10-03 19:50:08 +0100

    reformat class

--- data/network/src/main/java/com/frezzcoding/network/ads/AdDto.kt
+++ data/network/src/main/java/com/frezzcoding/network/ads/AdDto.kt
@@ -1,3 +1,9 @@
 package com.frezzcoding.network.ads
 
-data class AdDto(val id: Int?, val position: Int?, val text: String?, val adOwner: AdOwnerDto?, val video: String?)
+data class AdDto(
+    val id: Int?,
+    val position: Int?,
+    val text: String?,
+    val adOwner: AdOwnerDto?,
+    val video: String?
+)
