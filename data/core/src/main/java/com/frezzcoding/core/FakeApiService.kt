ebe672c80a8906df7a82fb7d3a890b5bf212aa73
oskarlasota <frezzyysmite@gmail.com>
2023-10-03 19:53:38 +0100

    created a fake api service and injecting it into repo

---
+++ data/core/src/main/java/com/frezzcoding/core/FakeApiService.kt
@@ -0,0 +1,18 @@
+package com.frezzcoding.core
+
+import com.frezzcoding.network.SampleData
+import com.frezzcoding.network.ads.AdDto
+import com.frezzcoding.network.quiz.QuizDto
+import javax.inject.Inject
+
+class FakeApiService @Inject constructor() {
+
+    fun fetchAds(): List<AdDto> {
+        return SampleData.advertisementsSample
+    }
+
+    fun fetchQuizzes() : List<QuizDto> {
+        return SampleData.quizSample
+    }
+
+}
--- features/home/core/src/main/java/com/frezzcoding/core/data/repository/HomeRepositoryImpl.kt
+++ features/home/core/src/main/java/com/frezzcoding/core/data/repository/HomeRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.frezzcoding.core.data.repository
 
 import com.frezzcoding.HomeRepository
+import com.frezzcoding.core.FakeApiService
 import com.frezzcoding.core.db.FeedDao
 import com.frezzcoding.core.mappers.AdMapper
 import com.frezzcoding.core.mappers.QuizMapper
@@ -12,17 +13,18 @@ import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class HomeRepositoryImpl @Inject constructor(
+    private val apiService: FakeApiService,
     private val adMapper: AdMapper,
     private val quizMapper: QuizMapper
 ) : HomeRepository {
 
     override fun getAds(): Flow<List<AdDetails>> {
-        val mappedData = SampleData.advertisementsSample.mapNotNull(adMapper::apply)
+        val mappedData = apiService.fetchAds().mapNotNull(adMapper::apply)
         return flowOf(mappedData)
     }
 
     override fun getQuizzes(): Flow<List<QuizDetails>> {
-        val mappedData = SampleData.quizSample.mapNotNull(quizMapper::apply)
+        val mappedData = apiService.fetchQuizzes().mapNotNull(quizMapper::apply)
         return flowOf(mappedData)
     }
 
