c87b68e1313ed03c570220eb741b3a6652d96e64
oskarlasota <frezzyysmite@gmail.com>
2023-10-27 23:11:59 +0100

    adding ksp dependency

--- data/core/src/main/java/com/frezzcoding/core/db/FeedDatabase.kt
+++ data/core/src/main/java/com/frezzcoding/core/db/FeedDatabase.kt
@@ -3,7 +3,7 @@ package com.frezzcoding.core.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [FeedItemEntity::class], version = 1)
+@Database(entities = [FeedItemEntity::class], version = 2, exportSchema = false)
 abstract class FeedDatabase : RoomDatabase() {
 
     abstract fun feedDao(): FeedDao
