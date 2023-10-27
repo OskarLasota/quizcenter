8f9c2342224f1665858909d8cf61217c736c5611
oskarlasota <frezzyysmite@gmail.com>
2023-10-27 21:48:22 +0100

    adding missing db functions

--- data/core/src/main/java/com/frezzcoding/core/db/FeedDao.kt
+++ data/core/src/main/java/com/frezzcoding/core/db/FeedDao.kt
@@ -1,12 +1,24 @@
 package com.frezzcoding.core.db
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface FeedDao {
 
     @Query("SELECT * FROM FeedItemEntity")
     suspend fun getFeed(): List<FeedItemEntity>
+
+    @Insert
+    suspend fun addItem(item: FeedItemEntity)
+
+    @Update
+    suspend fun updateItem(item: FeedItemEntity)
+
+    @Delete
+    suspend fun deleteItem(item: FeedItemEntity)
 
 }
