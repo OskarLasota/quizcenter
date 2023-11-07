8cf8faf9aad7dcf1ee0d7a794e0994a54a99bc64
oskarlasota <frezzyysmite@gmail.com>
2023-11-07 20:45:15 +0000

    renaming lambda function of video fully visible

--- presentation/core/src/main/java/com/frezzcoding/core/screens/home/HomeScreen.kt
+++ presentation/core/src/main/java/com/frezzcoding/core/screens/home/HomeScreen.kt
@@ -2,20 +2,11 @@ package com.frezzcoding.core.screens.home
 
 import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
 import com.frezzcoding.HomeUiState
 import com.frezzcoding.HomeViewModel
-import com.frezzcoding.domain.models.FeedItem
-import com.frezzcoding.domain.models.ad.AdDetails
 import com.frezzcoding.domain.models.quiz.QuizDetails
 import com.frezzcoding.ui.components.HomeFeed
 import kotlinx.collections.immutable.ImmutableList
@@ -31,27 +22,11 @@ fun HomeScreen(
     val state by homeViewModel.state.collectAsState(initial = HomeUiState())
     val displayList: ImmutableList<Any> = (state.ads + state.quizzes).toImmutableList()
 
-    var lifecycle by remember {
-        mutableStateOf(Lifecycle.Event.ON_CREATE)
-    }
-
-    val lifecycleOwner = LocalLifecycleOwner.current
-
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            lifecycle = event
-        }
-        lifecycleOwner.lifecycle.addObserver(observer)
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    }
     HomeFeed(
         items = displayList,
         player = homeViewModel.player,
-        lifecycle = lifecycle,
-        onItemFullyVisible = { item ->
-            onItemFullyVisible(item, homeViewModel)
+        onVideoItemFullyVisible = { item ->
+            onVideoItemFullyVisible(item, homeViewModel)
         },
         onItemPressed = { onAdPressed(navController, homeViewModel) }
     )
@@ -59,16 +34,17 @@ fun HomeScreen(
     homeViewModel.playVideo(null)
 }
 
-fun onItemFullyVisible(item: FeedItem?, viewModel: HomeViewModel) {
-    when(item) {
+fun onVideoItemFullyVisible(item: QuizDetails?, viewModel: HomeViewModel) {
+    when (item) {
         is QuizDetails -> {
-            Log.e("home screen", "quiz details visible")
+            Log.e("home screen", "quiz video ready to play")
             viewModel.playVideo(item)
         }
-        is AdDetails -> {
-            Log.e("home screen", "ad visible")
-        }
+
+        else -> {}
     }
 }
 
-fun onAdPressed(navController: NavController, homeViewModel: HomeViewModel) {}
+fun onAdPressed(navController: NavController, homeViewModel: HomeViewModel) {
+
+}
