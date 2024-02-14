5e1e0bc7c53398260f8dfadd0e5370500264909a
oskarlasota <frezzyysmite@gmail.com>
2024-02-14 22:06:00 +0000

    creating a accessible stateflow variable

--- features/createquiz/contract/src/main/java/com/frezzcoding/CreateQuizViewModel.kt
+++ features/createquiz/contract/src/main/java/com/frezzcoding/CreateQuizViewModel.kt
@@ -1,6 +1,9 @@
 package com.frezzcoding
 
+import kotlinx.coroutines.flow.StateFlow
+
 interface CreateQuizViewModel {
+    val uiState: StateFlow<CreateQuizUiState>
     fun onCreateQuizAction()
     fun onQuizScreenClosed()
 }
