dffc810f15c9f32dd1f4bf3492a13ee771cab253
oskarlasota <frezzyysmite@gmail.com>
2024-02-15 22:14:22 +0000

    add error state

--- features/createquiz/contract/src/main/java/com/frezzcoding/CreateQuizUiState.kt
+++ features/createquiz/contract/src/main/java/com/frezzcoding/CreateQuizUiState.kt
@@ -2,5 +2,6 @@ package com.frezzcoding
 
 sealed class CreateQuizUiState {
     data object LoadingState: CreateQuizUiState()
+    data class ErrorState(val errorMessage: String): CreateQuizUiState()
 
 }
