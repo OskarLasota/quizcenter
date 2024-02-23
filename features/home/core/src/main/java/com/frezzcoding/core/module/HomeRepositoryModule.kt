c510c7af24eadec3250ef6219ce4bf31bd1e8cd5
oskarlasota <frezzyysmite@gmail.com>
2024-02-23 14:34:05 +0000

    adding new dependency on firebase firestore

--- features/home/core/src/main/java/com/frezzcoding/core/module/HomeRepositoryModule.kt
+++ features/home/core/src/main/java/com/frezzcoding/core/module/HomeRepositoryModule.kt
@@ -2,10 +2,16 @@ package com.frezzcoding.core.module
 
 import com.frezzcoding.HomeRepository
 import com.frezzcoding.core.data.repository.HomeRepositoryImpl
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -13,5 +19,16 @@ internal abstract class HomeRepositoryModule {
 
     @Binds
     abstract fun bindsHomeRepository(homeRepositoryImpl: HomeRepositoryImpl): HomeRepository
+
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal object FirebaseDatabaseModule {
+
+    @Singleton
+    @Provides
+    fun providesFirebaseFirestore(): FirebaseFirestore =
+        Firebase.firestore
 
 }
