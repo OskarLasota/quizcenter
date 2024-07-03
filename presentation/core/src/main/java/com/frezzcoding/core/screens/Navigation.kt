6cc8a9cf956f4860ae0b9955e59523a605352b6a
oskarlasota <frezzyysmite@gmail.com>
2024-07-03 22:20:15 +0100

    remove unused import

--- presentation/core/src/main/java/com/frezzcoding/core/screens/Navigation.kt
+++ presentation/core/src/main/java/com/frezzcoding/core/screens/Navigation.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
