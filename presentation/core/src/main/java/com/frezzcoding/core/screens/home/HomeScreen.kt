b3e46048f2ab748260a621c151c93b91078807cb
oskarlasota <frezzyysmite@gmail.com>
2024-07-03 22:20:52 +0100

    remove unused import

--- presentation/core/src/main/java/com/frezzcoding/core/screens/home/HomeScreen.kt
+++ presentation/core/src/main/java/com/frezzcoding/core/screens/home/HomeScreen.kt
@@ -2,11 +2,9 @@ package com.frezzcoding.core.screens.home
 
 import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.frezzcoding.HomeUiState
 import com.frezzcoding.core.ui.HomeViewModelImpl
