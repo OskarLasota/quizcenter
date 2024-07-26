06efe35bed74a404ee9bdac10a4b8be6bd4945d9
oskarlasota <frezzyysmite@gmail.com>
2024-07-26 15:21:49 +0100

    Changing data structure from map to Pair

--- domain/src/main/java/com/frezzcoding/domain/models/quiz/QuizDetails.kt
+++ domain/src/main/java/com/frezzcoding/domain/models/quiz/QuizDetails.kt
@@ -5,7 +5,7 @@ import com.frezzcoding.domain.models.FeedItem
 data class QuizDetails(
     val id: Int,
     val description: String,
-    val content: Map<QuizQuestion, List<QuizAnswer>>,
+    val content: Pair<QuizQuestion, List<QuizAnswer>>,
     val owner: QuizOwner,
     val video: String?
 ) : FeedItem
